ab83c9d41da8c9b2b7f2a515b0e8cbc985baf2d0
Ronan <ronan.le_meillat@sctg.eu.org>
2023-02-16 17:20:54 +0000

    proxy GET/POST and headers

--- hcf/proxy/src/index.ts
+++ hcf/proxy/src/index.ts
@@ -1,17 +1,21 @@
+import type {Request} from "@cloudflare/workers-types"
 export interface Env {
 	OKTETO_FQDN_HCFMAILER: string
 }
 
 export default {
 	async fetch(
-		request: Request,
+		request: Request<unknown>,
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const url = new URL(request.url);
 		url.hostname = env.OKTETO_FQDN_HCFMAILER;
-		console.log(`Proxy to: ${url.toString()}`)
-		const data = await fetch(url.toString(),request.cf as RequestInit<RequestInitCfProperties>);
+		console.log(`Proxy to:${request.method} ${url.toString()}`)
+		request.headers.forEach((value,key) => {
+			console.log(`\t${key}:${value}`)
+		})
+		const data = await fetch(url.toString(),request as RequestInit<RequestInitCfProperties>);
 		return data;
 	},
 };
